fab4c7af65a86b4d7202c0ecabeecb24ae3e1b38
lievi <lievi.silva@gmail.com>
2018-06-23 19:25:43 -0300

    s4a18 - Criando módulos

--- app.js
+++ app.js
@@ -1,28 +1,2 @@
-// Function Statment
-
-function greet() {
-    console.log('Hello World');
-}
+var greet = require('./greet');
 greet();
-
-//Functions are first-class
-function logGreeting(fn) {
-    fn();
-}
-
-logGreeting(greet);
-
-// Function expression
-var greetMe = function () {
-    console.log('Hello Lievi');
-}
-greetMe();
-
-// Function expression is still first-class
-logGreeting(greetMe);
-
-// Use a functions expression on the fly
-
-logGreeting(function () {
-    console.log('Hello Lievi on the fly');
-})
