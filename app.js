0160bd2aea5a2ca21c8de6273fc00e4b177f27aa
lievi <lievi.silva@gmail.com>
2018-06-24 09:39:23 -0300

    s4a19 - Objects and Literal Objects

--- app.js
+++ app.js
@@ -1,2 +1,10 @@
-var greet = require('./greet');
-greet();
+var person = {
+    firstName: 'John',
+    lastName: 'Doe',
+    greet: function () {
+        console.log('Hello, ' + this.firstName + ' ' + this.lastName);
+
+    }
+}
+person.greet();
+console.log(person['firstName']);
