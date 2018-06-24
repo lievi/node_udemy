406ce7dab0b20ad13316e2a93eb14277fe70cfab
lievi <lievi.silva@gmail.com>
2018-06-24 10:47:36 -0300

    s4a20 Prototypes

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
-var person = {
-    firstName: 'John',
-    lastName: 'Doe',
-    greet: function () {
-        console.log('Hello, ' + this.firstName + ' ' + this.lastName);
-
-    }
+function Person(firstName, lastName) {
+    this.lastName = lastName;
+    this.firstName = firstName;
 }
-person.greet();
-console.log(person['firstName']);
+
+Person.prototype.greet = function () {
+    console.log('Hello, ' + this.firstName);
+}
+var john = new Person('John', 'Doe');
+var jane = new Person('Jane', 'Doe');
+john.greet();
+jane.greet();
+
+console.log(john.__proto__);
+
