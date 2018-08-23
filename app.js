5e59f43bfdd09dc07af1c914477cbc40400658a3
lievi <lievi.silva@gmail.com>
2018-08-22 21:30:11 -0300

    aula 21

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
-var firstName = 'Jane';
-(function () {
-    var firstName = 'John';
-    console.log(firstName);
-}());
-console.log(firstName);
+// pass by value
+function change(b) {
+    b = 2;
+}
+
+var a = 1;
+change(a);
+console.log(a);
+
+//passing by reference
+
+function changeObj(d) {
+    d.prop1 = function() {};
+    d.prop2 = {};
+}
+
+var c = {};
+c.prop1 = {};
+changeObj(c);
+console.log(c);
