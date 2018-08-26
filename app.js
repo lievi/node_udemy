958d6b36d855a5d496f305182006c729fd19b38f
lievi <lievi.silva@gmail.com>
2018-08-25 21:49:21 -0300

    aula 34

--- app.js
+++ app.js
@@ -1,27 +1,15 @@
-var Emitter = require("./emitter");
+var Emitter = require("events");
+var eventConfig = require("./config").events;
 
 var emtr = new Emitter();
 
-emtr.on("greet", function() {
+emtr.on(eventConfig.GREET, function() {
     console.log("Somewhere, someone said hello.");
 });
 
-emtr.on("greet", function() {
+emtr.on(eventConfig.GREET, function() {
     console.log("A greeting occurred!");
 });
 
-emtr.on("xesque", function() {
-    console.log("dale");
-});
-
-emtr.on("xesque", function() {
-    console.log("dele");
-});
-
-emtr.on("xesque", function() {
-    console.log("dolly");
-});
-
 console.log("Hello!");
-emtr.emit("greet");
-emtr.emit("xesque");
+emtr.emit(eventConfig.GREET);
