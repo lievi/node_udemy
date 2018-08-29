da6c3a95f4a616c5a877d262ad1711ec91f5b378
lievi <lievi.silva@gmail.com>
2018-08-28 22:33:44 -0300

    aula 36

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
-var person = {
-    firstname: "",
-    lastname: "",
-    greet: function() {
-        return this.firstname + " " + this.lastname;
-    }
+var EventEmitter = require("events");
+var util = require("util");
+
+function Greetr() {
+    this.greeting = "Hello World";
+}
+
+util.inherits(Greetr, EventEmitter);
+
+Greetr.prototype.greet = function(data) {
+    console.log(this.greeting + ": " + data);
+    this.emit("greet", data);
 };
 
-var john = Object.create(person);
-john.firstname = "John";
-john.lastname = "Doe";
+var greeter1 = new Greetr();
+greeter1.on("greet", function(data) {
+    console.log("Someone greeted: " + data);
+});
 
-var jane = Object.create(person);
-jane.firstname = "Jane";
-jane.lastname = "Doe";
-
-console.log(john.greet());
-console.log(jane.greet());
+greeter1.greet("Lievi");
