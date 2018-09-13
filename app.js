fd69e44d760f3264092c648edddb7f8dfaa7cb8e
lievi <lievi.silva@gmail.com>
2018-09-12 21:43:14 -0300

    60 - Template

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 var http = require("http");
+var fs = require("fs");
 http.createServer(function(req, res) {
-    res.writeHead(200, { "Content-Type": "text/plain" });
-    res.end("Hello World\n");
+    res.writeHead(200, { "Content-Type": "text/html" });
+    var html = fs.readFileSync(__dirname + "/index.html", "utf-8");
+    var message = "Hello World....";
+    html = html.replace("{Message}", message);
+    res.end(html);
 }).listen(1337, "localhost");
