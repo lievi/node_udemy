a5dc9cca4e189f518100ba992a2bd7fceebada7f
lievi <lievi.silva@gmail.com>
2018-09-17 21:17:23 -0300

    Aula 63 - Send Data

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 var http = require("http");
 var fs = require("fs");
 http.createServer(function(req, res) {
-    res.writeHead(200, { "Content-Type": "text/html" });
-    fs.createReadStream(__dirname + "/index.html", "utf-8").pipe(res);
+    res.writeHead(200, { "Content-Type": "application/json" });
+    var obj = {
+        firstname: "John",
+        lastName: "Doe"
+    };
+    res.end(JSON.stringify(obj));
 }).listen(1337, "localhost");
