1e45cf44df26b4740d228722e3f9ac34bb8827cb
lievi <lievi.silva@gmail.com>
2018-09-17 21:02:41 -0300

    61 - Streams on response

--- app.js
+++ app.js
@@ -2,8 +2,5 @@ var http = require("http");
 var fs = require("fs");
 http.createServer(function(req, res) {
     res.writeHead(200, { "Content-Type": "text/html" });
-    var html = fs.readFileSync(__dirname + "/index.html", "utf-8");
-    var message = "Hello World....";
-    html = html.replace("{Message}", message);
-    res.end(html);
+    fs.createReadStream(__dirname + "/index.html", "utf-8").pipe(res);
 }).listen(1337, "localhost");
